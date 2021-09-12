b12b8821fe84d476960898a67181d8fe4f60d613
Daniel Hohl <danielhohl@gmail.com>
2021-09-12 14:36:44 -0300

    Rev. Erros 3

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -21,8 +21,7 @@ const Home = () => {
                     Acessar o dashboard
                 </Link>
             </div>
-        </div>
-        
+        </div>       
         <Footer />
         </>
     );
