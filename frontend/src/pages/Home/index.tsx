6f6d6af2fdff0e7800f6639227deee59d85f222e
Daniel Hohl <danielhohl@gmail.com>
2021-09-12 17:54:54 -0300

    Rev. Erros 10

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -15,14 +15,24 @@ const Home = () => {
             <div className="jumbotron">
                 <h1 className="display-4">DSVendas</h1>
                 <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
-                <hr />
+                <hr/>
                 <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                 <Link className="btn btn-primary btn-lg" to="/dashboard">
                     Acessar o dashboard
                 </Link>
             </div>
-        </div>       
+        </div> 
+        <div className="div">
+                <tr>
+                    <td>
+                         
+                    </td>
+                </tr>
+        </div>
+        <div>
         <Footer />
+            </div>      
+        
         </>
     );
 }
